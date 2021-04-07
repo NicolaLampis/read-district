79fc6432d28c050de40fd25181643f53fdfd48ef
NicolaLampis <nico.lampo@gmail.com>
2021-04-07 20:19:27 +0000

    Add register functionality, flash message injected into base.html, werkzeug password hash

--- app.py
+++ app.py
@@ -28,8 +28,21 @@ def get_books():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "email": request.form.get("email")
+        }
+        mongo.db.users.insert_one(register)
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
-
 
 
 if __name__ == "__main__":
