f3086f298d01cf9a440a70e9fe01e775008752e2
NicolaLampis <nico.lampo@gmail.com>
2021-04-12 20:46:10 +0000

    style the search bar

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('select').formSelect();
+    $('textarea#text_review').characterCounter();
+
+    $('.materialboxed').materialbox();
 });
 
