58b00cd58dc7cfdd497f688887a0735a581101f8
NicolaLampis <nico.lampo@gmail.com>
2021-04-13 22:53:06 +0000

    Style books.html, cards with same height, floating buttons edit/delete, wave button animation

--- static/js/script.js
+++ static/js/script.js
@@ -4,5 +4,12 @@ $(document).ready(function(){
     $('textarea#text_review').characterCounter();
 
     $('.materialboxed').materialbox();
+
+    $('.my-action-btn').floatingActionButton({
+        hoverEnabled: false,
+        direction: 'top',
+        isOpen: true
+    });
+
 });
 
