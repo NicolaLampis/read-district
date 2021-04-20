faad8aca19e2a7667df6ea992584600406625bf0
NicolaLampis <nico.lampo@gmail.com>
2021-04-20 08:22:12 +0000

    format javascript file and comment code

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,12 @@
+// Materialize CSS jQuery initialisation script
+
 $(document).ready(function(){
+    // Materialize plugins: mobile-side-navigation
     $('.sidenav').sidenav({edge: "right"});
+    // Materialize form
     $('select').formSelect();
+    // Materialize text form with character counter
     $('textarea#text_review').characterCounter();
-
-    $('.materialboxed').materialbox();
-
+    // Materialize Modal window    
     $('.modal').modal();
-
 });
